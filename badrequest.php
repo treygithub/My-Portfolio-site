cd49f2902ff93b0a5e05f59cf03c92dcf26bba91
robert hunnicutt <robhunn74@gmail.com>
2019-02-09 14:41:00 -0600

    My profile web page is complete - gitignore mylibray folder for db con

---
+++ badrequest.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>attack made</title>
+    <style>
+        .pisser{
+            margin:auto;
+        }
+        img {
+            position: absolute;
+            left: 400px;
+            top: 50px;
+        }
+    </style>
+</head>
+<body>
+
+<?php
+$ip1 = $_SERVER['REMOTE_ADDR'];
+$port1 = $_SERVER['REMOTE_PORT'];
+$hostname1 = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+$text1 = " ip address is: " . $ip1 . " " . "user port: " . $port1 . " " . "user host: " . $hostname1;
+$file1 = fopen('attack.html', 'a');
+fwrite($file1, $text1 . " " . date("F j, Y, g:i a") . "<hr><br/>\n");
+fclose($file1);
+?>
+
+
+    <div class="pisser">
+        <img src="./images/400.jpg" width="750px" height="600px" title="piss off" alt="unauthorized">
+    </div>
+</body>
+</html>
